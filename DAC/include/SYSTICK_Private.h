89548bc905867b3adf97b6d16910df2e47e70287
islam mohamed negm <127314507+islamnegm@users.noreply.github.com>
2023-06-09 15:56:12 +0300

    Add files via upload

---
+++ DAC/include/SYSTICK_Private.h
@@ -0,0 +1,14 @@
+
+
+
+#ifndef SYSYICK_PRIVATE_H
+#define SYSYICK_PRIVATE_H
+
+#define SYS_TICK_SRC_AHB_8 5
+#define SYS_TICK_SRC_AHB   4
+
+
+#define STK_u8_PERIODIC_INTERVAL 1
+#define STK_u8_SINGLE_INTERVAL 2
+
+#endif
