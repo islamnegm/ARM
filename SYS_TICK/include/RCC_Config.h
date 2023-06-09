871ea419a086ebd9178ec20fed52faa8b35732af
islam mohamed negm <127314507+islamnegm@users.noreply.github.com>
2023-06-09 16:06:17 +0300

    Add files via upload

---
+++ SYS_TICK/include/RCC_Config.h
@@ -0,0 +1,16 @@
+
+
+
+#ifndef RCC_CONFIG_H
+#define RCC_CONFIG_H
+
+
+/* option 1-RCC_u8_HSI
+ *        2-RCC_u8_HSE_OSC
+ *        3-RCC_u8_PLL
+ *        4-RCC_u8_HSE_BYBASS
+ */
+#define RCC_u8_CLC_TYPE RCC_u8_HSI
+
+
+#endif
