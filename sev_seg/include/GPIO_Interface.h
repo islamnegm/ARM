f2dfb58c05b7e45c017cf221a625db42d0fd3470
islam mohamed negm <127314507+islamnegm@users.noreply.github.com>
2023-06-09 16:00:58 +0300

    Add files via upload

---
+++ sev_seg/include/GPIO_Interface.h
@@ -0,0 +1,16 @@
+
+
+
+#ifndef GPIO_INTERFACE_H
+#define GPIO_INTERFACE_H
+
+
+void GPIO_void_SetPinMode(u8 copy_u8GPIOX ,u8 copy_u8PIN, u8 copy_Pin_Mode);
+void GPIO_void_SetPinType(u8 copy_u8GPIOX ,u8 copy_u8PIN,u8 copy_PinType);
+void GPIO_void_SetPinSpeed(u8 copy_u8GPIOX ,u8 copy_u8PIN,u8 copy_PinSpeed );
+void GPIO_void_SetPinValue(u8 copy_u8GPIOX ,u8 copy_u8PIN,u8 copy_Value);
+
+
+
+
+#endif
