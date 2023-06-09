1b8d91d35d069d77c3cdac1e507c1f433a31f84d
islam mohamed negm <127314507+islamnegm@users.noreply.github.com>
2023-06-09 15:57:01 +0300

    Add files via upload

---
+++ EXTI_Ass/include/EXTI_Config.h
@@ -0,0 +1,8 @@
+
+
+#ifndef EXTI_CONFIG_H_
+#define EXTI_CONFIG_H_
+
+
+
+#endif /* EXTI_CONFIG_H_ */
---
+++ EXTI_Ass/include/SYSTICK_Private.h
@@ -0,0 +1,10 @@
+
+
+
+#ifndef SYSYICK_PRIVATE_H
+#define SYSYICK_PRIVATE_H
+
+#define SYS_TICK_SRC_AHB_8 5
+#define SYS_TICK_SRC_AHB   4
+
+#endif
